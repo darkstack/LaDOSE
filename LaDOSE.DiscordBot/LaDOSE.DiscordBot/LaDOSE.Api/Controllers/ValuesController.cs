3b88ea1d4803a3c28138515a17bec0aa12096681
Darkstack <1835601+darkstack@users.noreply.github.com>
2018-10-04 21:13:32 +0200

    Api Project

---
+++ LaDOSE.DiscordBot/LaDOSE.DiscordBot/LaDOSE.Api/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LaDOSE.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConfigController : ControllerBase
+    {
+        // GET api/Config
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/Config/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+    }
+}
